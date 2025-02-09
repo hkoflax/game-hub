480d55d07104338047ac44e0db2719e64f0e0feb
koflax.hounkpe <koflax.hounkpe@assomption.ca>
2025-02-09 09:22:12 -0400

    build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
-    <>
-      <Button colorPalette="blue">Button</Button>{" "}
-    </>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+
+      <GridItem hideBelow="lg" area="aside" bg="gold">
+        Aside
+      </GridItem>
+
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
