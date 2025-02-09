4e3382e34dacaefdc6292440da33a2ad21cb0e7e
koflax.hounkpe <koflax.hounkpe@assomption.ca>
2025-02-09 12:01:15 -0400

    Displaying Critics score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,7 @@
 import { Game } from "@/hooks/useGames";
-import { Card, Heading, Image, Text } from "@chakra-ui/react";
-import { GiPlatform } from "react-icons/gi";
+import { Card, Heading, HStack, Image } from "@chakra-ui/react";
 import PlatformIcon from "./PlatformIcon";
+import CriticScore from "./CriticScore";
 
 interface Props {
   game: Game;
@@ -12,9 +12,12 @@ export default function GameCard({ game }: Props) {
       <Image src={game.background_image} />
       <Card.Body>
         <Heading fontSize="2xl">{game.name}</Heading>
-        <PlatformIcon
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
+        <HStack justifyContent={"space-between"}>
+          <PlatformIcon
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
       </Card.Body>
       <Card.Footer />
     </Card.Root>
