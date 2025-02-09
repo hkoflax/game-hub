ac0f83f9281c843f1897f4bd72a1f4743936b70f
koflax.hounkpe <koflax.hounkpe@assomption.ca>
2025-02-09 15:56:49 -0400

    Show spinner while fetching genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,10 +1,18 @@
-import useGenres, { Genre } from "@/hooks/useGenre";
+import useGenres from "@/hooks/useGenre";
 import getCroppedImageUrl from "@/services/image-url";
-import { HStack, Image, ListItem, ListRoot, Text } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  ListItem,
+  ListRoot,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 
 export default function GenreList() {
-  const { data } = useGenres();
-
+  const { data, isLoading, error } = useGenres();
+  if (isLoading) return <Spinner />;
+  if (error) return null;
   return (
     <ListRoot variant="plain">
       {data.map((genre) => (
