d6d25ba0cd6865874b70ecd2a87c1071b06e3ae2
koflax.hounkpe <koflax.hounkpe@assomption.ca>
2025-02-09 09:54:48 -0400

    Add default theme

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import { ColorModeButton } from "./ui/color-mode";
 
 export default function NavBar() {
   return (
     <HStack>
       <Image src={logo} boxSize="60px" />
-      <Text>NavBar</Text>
+      <ColorModeButton />
     </HStack>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider>
+    <Provider defaultTheme="dark">
       <App />
     </Provider>
   </StrictMode>
